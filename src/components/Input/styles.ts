182cb25ebd58b55b46152a90601f15fde2f51f23
recofka <recofka@gmail.com>
2020-09-06 22:04:59 +0200

    setting style for inout when focus and-or blur

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
   border-radius: 10px;
-  border: 2px solid #233229;
   padding: 16px;
   width: 100%;
   background: #233229;
+
+  border: 2px solid #233129;
   color: #666360;
 
   & + div {
     margin-top: 10px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     flex: 1;
