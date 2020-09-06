e50c03e3924294c5e0d8aece3c35d28875aa824b
recofka <recofka@gmail.com>
2020-09-06 14:51:59 +0200

    isolating two components: input and button

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -25,42 +25,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #233229;
-      border-radius: 10px;
-      border: 2px solid #233229;
-      padding: 16px;
-      width: 100%;
-
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 10px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      width: 100%;
-      margin: 24px 0;
-      transition: background-color 0.2s;
-
-      color: #312e38;
-      font-weight: 500;
-
-      &:hover {
-        background: #c06d02;
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
