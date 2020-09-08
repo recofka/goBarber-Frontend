31be904d9e33d08db3aafdbd9e7afbf2f262b9da
recofka <recofka@gmail.com>
2020-09-08 20:33:58 +0200

    Styling input error

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,32 +1,37 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
 const Signup: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Name is required'),
         email: Yup.string()
           .required('E-mail is required')
           .email('Inform a valid e-mail address'),
-        password: Yup.string().min(
-          6,
-          'Enter a password of at least six characters',
-        ),
+        password: Yup.string().min(6, 'Password is required'),
       });
 
       await schema.validate(data, {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -37,7 +42,7 @@ const Signup: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Create your account</h1>
           <Input icon={FiUser} name="name" placeholder="Name" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
